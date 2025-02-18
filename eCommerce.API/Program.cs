f6940fe1aa6be2d21b6df8b31e13768e49d4812f
Nithushan UCA <nithshan@unicornconnectedapps.com>
2025-02-18 14:45:10 +0530

    initial

---
+++ eCommerce.API/Program.cs
@@ -0,0 +1,49 @@
+using eCommerceInfrastucture;
+using eCommerce.Core;
+using eCommerce.API.MiddleWares;
+using System.Text.Json.Serialization;
+using eCommerce.Core.Mapper;
+using FluentValidation.AspNetCore;
+var builder = WebApplication.CreateBuilder(args);
+
+//before build add infrastucture service 
+builder.Services.AddInfrastructure();
+builder.Services.AddCore();
+
+//add the controller to the service collection
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+builder.Services.AddAutoMapper(typeof(ApplicationUserMappingProfile).Assembly);
+//Fluent Validation
+builder.Services.AddFluentValidationAutoValidation();
+//add api explore services
+builder.Services.AddEndpointsApiExplorer();
+//add swagger genreration services
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4200").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+var app = builder.Build();
+app.UseExeptionHandlingMiddleWare();
+
+//Routing 
+app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors();
+
+//Auth
+app.UseAuthentication();
+app.UseAuthorization();
+
+//controller route
+app.MapControllers();
+
+app.Run();
