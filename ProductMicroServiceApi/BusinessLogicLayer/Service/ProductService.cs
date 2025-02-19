50947e89c8a113a48f22bb9545fdb6fff75a1d53
Nithushan Janson <jansonnithusanofficial@gmail.com>
2025-02-19 23:31:22 +0530

    added all service

--- ProductMicroServiceApi/BusinessLogicLayer/Service/ProductService.cs
+++ ProductMicroServiceApi/BusinessLogicLayer/Service/ProductService.cs
@@ -51,26 +51,51 @@ public class ProductService : IproductService
 
     public async Task<bool> DeleteProduct(Guid ProductID)
     {
-        throw new NotImplementedException();
+       Products? ExitingProducts = await _Repository.GetProductByCondition(temp => temp.ProductID == ProductID);
+        if (ExitingProducts == null) return false;
+
+        bool isDeleted = await _Repository.DeleteProduct(ProductID);
+        return isDeleted;
     }
 
     public async Task<ProductResponse?> GetProductByCondition(Expression<Func<Products, bool>> connditionExpression)
     {
-        throw new NotImplementedException();
+       Products? ExitingProducts = await _Repository.GetProductByCondition(connditionExpression);
+        if (ExitingProducts == null) return null;
+        ProductResponse response = _Mapper.Map<ProductResponse>(ExitingProducts);
+        return response;
     }
 
     public async Task<List<ProductResponse>> GetProducts()
     {
-        throw new NotImplementedException();
+        IEnumerable<Products?> products = await _Repository.GetProducts();
+        IEnumerable<ProductResponse?> ProductResponse = _Mapper.Map<IEnumerable<ProductResponse>>(products);
+        return  ProductResponse.ToList();
     }
 
     public async Task<List<ProductResponse?>> GetProductsByCondition(Expression<Func<Products, bool>> connditionExpression)
     {
-        throw new NotImplementedException();
+       IEnumerable<Products?> Products = await _Repository.GetProductsByCondition(connditionExpression);
+        IEnumerable<ProductResponse?> ProductResponse = _Mapper.Map<IEnumerable<ProductResponse>>(Products);
+        return ProductResponse.ToList();
     }
 
     public async Task<ProductResponse?> UpdateProduct(ProductUpdateRequest product)
     {
-        throw new NotImplementedException();
+        Products? ExitingProducts = await _Repository.GetProductByCondition(temp => temp.ProductID == product.ProductID);
+        if (ExitingProducts == null) throw new ArgumentException("Product Not Found");
+
+       FluentValidation.Results.ValidationResult validation = await _UpdateRequestValidator.ValidateAsync(product);
+
+        if (!validation.IsValid)
+        {
+            string errors = string.Join(",", validation.Errors.Select(temp => temp.ErrorMessage));
+            throw new ArgumentException(errors);
+        }
+        Products ProductMap = _Mapper.Map<Products>(product);
+        Products? UpdatedProducts = await _Repository.UpdateProduct(ProductMap);
+        if (UpdatedProducts == null) return null;
+        ProductResponse response = _Mapper.Map<ProductResponse>(UpdatedProducts);
+        return response;
     }
 }
