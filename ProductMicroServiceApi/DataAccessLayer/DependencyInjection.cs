20034cee6d23f1c4b6116c6d81a519eaec97a4e6
Nithushan Janson <jansonnithusanofficial@gmail.com>
2025-02-26 10:39:25 +0530

    its working finally but some data base issues

--- ProductMicroServiceApi/DataAccessLayer/DependencyInjection.cs
+++ ProductMicroServiceApi/DataAccessLayer/DependencyInjection.cs
@@ -16,12 +16,30 @@ namespace DataAccessLayer
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddDataAccessLayer(this IServiceCollection services,IConfiguration configuration)
+        public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
         {
+            //  string connectionStringTemplate = configuration.GetConnectionString("DefaultConncetion")!;
+            //string conencitonstring =  connectionStringTemplate.Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST"))
+            //      .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD"));
 
-            services.AddScoped<IproductRepository , ProductRepository>();
+            //  services.AddScoped<IproductRepository, ProductRepository>();
+            //  services.AddDbContext<ApplicationDbContext>(options =>
+            //  options.UseMySQL(conencitonstring));
+
+            string connectionStringTemplate = configuration.GetConnectionString("DefaultConnection")!;
+
+            // Ensure environment variables are not null
+            string mysqlHost = Environment.GetEnvironmentVariable("MYSQL_HOST") ?? "localhost";
+            string mysqlPassword = Environment.GetEnvironmentVariable("MYSQL_PASSWORD") ?? "261412";
+
+            // Replace placeholders with environment variables
+            string connectionString = connectionStringTemplate
+                .Replace("$MYSQL_HOST", mysqlHost)
+                .Replace("$MYSQL_PASSWORD", mysqlPassword);
+
+            services.AddScoped<IproductRepository, ProductRepository>();
             services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseMySQL(configuration.GetConnectionString("DefaultConncetion")!));
+                options.UseMySQL(connectionString));
             return services;
         }
     }
